f227f3314922025b344e95aaf00f214cdd94ae75
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-07-09 10:14:53 +0200

    remove AssetStudio backend due to UnityCsReference use

--- TypeTreeGeneratorCLI/Program.cs
+++ TypeTreeGeneratorCLI/Program.cs
@@ -14,7 +14,7 @@ class Program
 
         var backendOption = new Option<string>(
             aliases: ["--backend", "-b"],
-            description: "The backend to use (AssetStudio, AssetsTools, AssetRipper)",
+            description: "The backend to use (AssetsTools, AssetRipper)",
             getDefaultValue: () => "AssetsTools"
         );
 
